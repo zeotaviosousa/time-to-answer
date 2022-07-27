e2008c1db9165e7f472da00bb674783181cca542
otavosousa <otavioklearskin@gmail.com>
2022-07-27 08:50:49 -0300

    install and configure bootstrap/jquery

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,12 +2,16 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-
+import jQuery from 'jquery';
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "popper.js"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+window.$ = jQuery
+
+import "../base_application"
