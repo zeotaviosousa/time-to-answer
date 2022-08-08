025ac6517c0f00582e7ffc18fa8bdea190e3dcc2
otavosousa <otavioklearskin@gmail.com>
2022-08-08 08:29:07 -0300

    precompiler assets and javascripts files

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,9 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+# app/assets
+Rails.application.config.assets.precompile += %w( admin_backoffice.css admin_backoffice.js )
+
+# lib/assets
+Rails.application.config.assets.precompile += %w( sb-admin-2.min.css sb-admin-2.min.js )
